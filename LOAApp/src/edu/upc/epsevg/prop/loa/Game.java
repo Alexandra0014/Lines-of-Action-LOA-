14f8c3c6a3b0d06075c5024056cf7a23971feb30
Omara <omaraaibar2000@gmail.com>
2021-11-27 17:56:17 +0100

    Clase creada

--- LOAApp/src/edu/upc/epsevg/prop/loa/Game.java
+++ LOAApp/src/edu/upc/epsevg/prop/loa/Game.java
@@ -21,8 +21,8 @@ public class Game {
             @Override
             public void run() {
                 
-                //IPlayer player1 = new HumanPlayer("Octopus");
-                IPlayer player1 = new RandomPlayer("Crazy Chris");
+                IPlayer player1 = new HumanPlayer("Octopus");
+                //IPlayer player1 = new RandomPlayer("Crazy Chris");
                 IPlayer player2 = new RandomPlayer("Crazy Ivan");
                                 
                 new Board(player1 , player2, 4, Level.DIFFICULT);
---
+++ LOAApp/src/edu/upc/epsevg/prop/loa/players/NotCheckers.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.upc.epsevg.prop.loa.players;
+
+import edu.upc.epsevg.prop.loa.GameStatus;
+import edu.upc.epsevg.prop.loa.IAuto;
+import edu.upc.epsevg.prop.loa.IPlayer;
+import edu.upc.epsevg.prop.loa.Move;
+
+/**
+ *
+ * @author aibar
+ */
+public class NotCheckers implements IPlayer, IAuto {
+
+    @Override
+    public Move move(GameStatus gs) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void timeout() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String getName() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
